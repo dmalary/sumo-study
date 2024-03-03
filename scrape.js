7306173c225cdc9c7e257827ec28b0341764fe03
madhat5 <davidmalary@gmail.com>
2024-03-03 16:03:03 -0500

    feat: add functions - url generator, scraper

--- scrape.js
+++ scrape.js
@@ -1,4 +1,33 @@
 import { gotScraping } from 'got-scraping';
 import * as fs from "fs";
 
-// import {  } from './constants.js';
+import { years, bashoNum } from './constants.js';
+
+const url = (year, bashoNum) => {
+  return `https://sumodb.sumogames.de/Banzuke.aspx?b=${year}${bashoNum}#M`
+}
+// url() test
+// console.log(url(2007, 11))
+
+const scrape = (url, name) => {
+  (async () => {
+    try {
+      gotScraping
+        .get(url)
+        .then( ({ body }) => 
+          // console.log(`body`, body)
+
+          fs.writeFileSync(`./data/pages/${name}.txt`, body)
+        )
+    } catch (err) {
+      if (err) console.log(err.res.body)
+    }
+  })();
+}
+// scrape test
+scrape('https://sumodb.sumogames.de/Banzuke.aspx?b=200711#M', 'test')
+
+// const run = () => {
+
+// }
+// run()
